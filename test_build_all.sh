6177aaddbace3c07ef9de74028b1a9a70b7df65b
LoneWanderer-GH <lonewandererdev@gmail.com>
2021-11-28 17:25:03 +0100

    Classic/BCC : ElvUI 2.30 fix + toc bump

--- test_build_all.sh
+++ test_build_all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 addon_dir_path="Interface/AddOns/"
+echo ${WOW_ROOT_LINUX}
 # ./packager/release.sh -r ${WOW_ROOT}/__classic_era__/Interface\AddOns/CoolLine -e -g classic -d -l -z
 # ./packager/release.sh -r ${WOW_ROOT}/__classic__/Interface/AddOns/CoolLine     -e -g bcc     -d -l -z
 # ./packager/release.sh -r ${WOW_ROOT}/__retail__/Interface/AddOns/CoolLine      -e -g retail  -d -l -z
